cfb541e932a62c1d5e6d403465e2d8b5cdb97b6b
begies <44854126+begies@users.noreply.github.com>
2025-04-28 04:07:55 +0000

    working template

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,12 +1,46 @@
-/******************************************************/
-// fb_initialise()
-// Called by html initialise button
-// Initialise the Firebase Database
-//   Initialise the firebase config object
-//   Get a database
-// Input:  n/a
-// Return: n/a
-/******************************************************/
+//**************************************************************/
+// fb_io.mjs
+// Generalised firebase routines
+// Written by <Your Name Here>, Term 2 202?
+//
+// All variables & function begin with fb_  all const with FB_
+// Diagnostic code lines have a comment appended to them //DIAG
+/**************************************************************/
+const FB_COL_C = 'white';
+const FB_COL_B = '#CD7F32';
+console.log('%c fb_io.mjs',
+            'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the methods you want to call from the firebase modules
+
+
+/**************************************************************/
+// EXPORT FUNCTIONS
+// List all the functions called by code or html outside of this module
+/**************************************************************/
+export { 
+    fb_initialise, fb_login, fb_authChanged, fb_logout, 
+    fb_writeRec, fb_readRec, fb_readAll, 
+    fb_updateRec, fb_readSorted,
+    
+};
+
 function fb_initialise() {
-    console.log('%c fb_initialise(): ', 'color: ' + FB_COL_C + '; background-color: ' + FB_COL_B + ';')
+    console.log('%c fb_initialise(): ', 
+                'color: ' + FB_COL_C + '; background-color: ' + FB_COL_B + ';');
 }
+function fb_login() {}
+function fb_authChanged() {}
+function fb_logout() {}
+function fb_writeRec() {}
+function fb_readRec() {}
+function fb_readAll() {}
+function fb_updateRec() {}
+function fb_readSorted() {}
+
+/**************************************************************/
+// END OF CODE
+/**************************************************************/
--- main.mjs
+++ main.mjs
@@ -1,13 +1,35 @@
 /**************************************************************/
 // main.mjs
-// Written by <Your Name Here>, Term 2 202?
 // Main entry for index.html
+// Written by <Your Name Here>, Term 2 202?
 /**************************************************************/
-
 const FB_COL_C = 'white';	//These two const are part of the coloured 	
 const FB_COL_B = '#CD7F32';	//console.log for functions scheme
-console.log('%c main.mjs', 'color: blue; background-color: white;');
+console.log('%c main.mjs', 
+    'color: blue; background-color: white;');
 
-import { fb_initialise() }
-console.log('%c poop', 'color: blue; background-color: white;');
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from fb_io module
+import { fb_initialise, fb_login, fb_authChanged, fb_logout, 
+         fb_writeRec, fb_readRec, fb_readAll, fb_updateRec, fb_readSorted }
+    from './fb_io.mjs';
+    window.fb_initialise   = fb_initialise;
+    window.fb_login        = fb_login;
+    window.fb_authChanged  = fb_authChanged;
+    window.fb_logout       = fb_logout;
+    window.fb_writeRec     = fb_writeRec;
+    window.fb_readRec      = fb_readRec;
+    window.fb_updateRec    = fb_updateRec;
+    window.fb_readAll      = fb_readAll;
+    window.fb_readSorted   = fb_readSorted;
+    
+/**************************************************************/
+// index.html main code
+/**************************************************************/
 
+
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
