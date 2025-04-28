3529d042988c5cd4cd207aaedb0fd2cbcd2ff284
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-04-28 03:28:34 +0000

    done

---
+++ fb_io.mjs
@@ -0,0 +1,12 @@
+/******************************************************/
+// fb_initialise()
+// Called by html initialise button
+// Initialise the Firebase Database
+//   Initialise the firebase config object
+//   Get a database
+// Input:  n/a
+// Return: n/a
+/******************************************************/
+function fb_initialise() {
+    console.log('%c fb_initialise(): ', 'color: ' + FB_COL_C + '; background-color: ' + FB_COL_B + ';')
+}
---
+++ main.mjs
@@ -0,0 +1,13 @@
+/**************************************************************/
+// main.mjs
+// Written by <Your Name Here>, Term 2 202?
+// Main entry for index.html
+/**************************************************************/
+
+const FB_COL_C = 'white';	//These two const are part of the coloured 	
+const FB_COL_B = '#CD7F32';	//console.log for functions scheme
+console.log('%c main.mjs', 'color: blue; background-color: white;');
+
+import { fb_initialise() }
+console.log('%c poop', 'color: blue; background-color: white;');
+
